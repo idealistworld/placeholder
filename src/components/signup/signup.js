0a6a8fd474634eb71c7eeee89462547a091305d7
Chris <65946592+idealistworld@users.noreply.github.com>
2022-11-21 14:39:55 -0500

    Add files via upload

---
+++ src/components/signup/signup.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../navbar/navbar'
+import InputSection from '../inputsection/inputsection'
+import './signup.css'
+
+const Signup = () => {
+    return (
+        <div id = "landing-container">
+            <Navbar></Navbar>
+            <InputSection></InputSection>
+        </div>
+    );
+}
+
+export default Signup;
